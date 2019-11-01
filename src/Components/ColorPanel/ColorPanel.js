f9793460c3bd238799d3415ab4fe0a51b463e98f
khadija hussain <hussainkhadija8@gmail.com>
2019-11-01 09:25:55 +0500

    colorPanel

--- src/Components/ColorPanel/ColorPanel.js
+++ src/Components/ColorPanel/ColorPanel.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
 
 class ColorPanel extends React.Component {
   render() {
-    return <div>ColorPanel</div>;
+    return (
+      <Sidebar
+        as={Menu}
+        Icon="labeled"
+        inverted
+        vertical
+        visible
+        width="very thin"
+      >
+        <Divider />
+      </Sidebar>
+    );
   }
 }
 export default ColorPanel;
